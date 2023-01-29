8ffe979e97c4fb4d388a6dfbd81db5b59884a5e0
lattecoff <lattecoff@lattecoff.com>
2023-01-29 17:01:22 +0300

    Переход на ветку porsche и удаление проекта для создание нового.

--- dj_project/dj_project/dj_app/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Carrera(models.Model):
-    power = models.IntegerField()
-    speed = models.IntegerField()
-    acceleration = models.FloatField
-    type_body = models.CharField(max_length=15)
